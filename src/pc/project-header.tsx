de80d01d50a0c070050f47ff011bf089b959500c
Gaurav <gauravcoder3512@gmail.com>
2025-12-04 14:43:22 +0530

    error

--- src/pc/project-header.tsx
+++ src/pc/project-header.tsx
@@ -24,7 +24,7 @@ const animation = {
 type ProjectHeaderProps = Project
 
 const ProjectHeader = (props: ProjectHeaderProps) => {
-  const { name, description, homepage, github } = props
+  const { name, homepage, github } = props
 
   const repo = github.split('/').pop()
 
